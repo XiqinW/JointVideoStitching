25dcfcdcb5e7bab398ac61a61093e9cd05caca67
kiiyvan <plocircle@live.com>
2018-10-30 21:10:49 +0800

    make all factors global variate

--- run.py
+++ run.py
@@ -2,7 +2,9 @@ import cv2
 import numpy as np
 
 FRAME_SIZE = (720, 1280, 3)
-FAST_FEATURE_THRESHOLD = 50
+GRID_ = (5, 5)
+FAST_FEATURE_THRESHOLD = [5 for i in range(GRID_[0] * GRID_[1])]
+MIN_FEATURE_NUM = 800
 
 
 def feature_detector(path):
@@ -12,17 +14,19 @@ def feature_detector(path):
     if frame.shape != FRAME_SIZE:
         frame = cv2.resize(frame, (FRAME_SIZE[1], FRAME_SIZE[0]), cv2.INTER_LINEAR)
 
-    fast = cv2.FastFeatureDetector_create(threshold=FAST_FEATURE_THRESHOLD, nonmaxSuppression=False,
-                                          type=cv2.FAST_FEATURE_DETECTOR_TYPE_9_16)
     # as paper said, " we divide a single frame into 5×5 regular grids
     # and for each grid we use an independent FAST feature detector."
-    grid_size = [int(FRAME_SIZE[0] / 5), int(FRAME_SIZE[1] / 5)]
+    grid_size = [int(FRAME_SIZE[0] / GRID_[0]), int(FRAME_SIZE[1] / GRID_[1])]
     kp = []
     frame_kp = frame.copy()
 
-    for i in range(5):
-        for j in range(5):
+    for i in range(GRID_[0]):
+        for j in range(GRID_[1]):
             mask = np.zeros(frame.shape, np.uint8)
+            threshold = FAST_FEATURE_THRESHOLD[i * GRID_[1] + j]
+            fast = cv2.FastFeatureDetector_create(threshold, nonmaxSuppression=True,
+                                                  type=cv2.FAST_FEATURE_DETECTOR_TYPE_9_16)
+
             mask[int(i * grid_size[0]):int((i + 1) * grid_size[0]),
             int(j * grid_size[1]):int((j + 1) * grid_size[1])] = 1
 
@@ -31,14 +35,13 @@ def feature_detector(path):
             cv2.drawKeypoints(frame_kp, grid_kp, frame_kp, color=(0, 255, 0))
             kp += grid_kp
 
-    cv2.imshow('frame', frame)
     cv2.imshow('frame_kp', frame_kp)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
 def main():
-    frame_path = "./building.jpg"
+    frame_path = "./0001.jpg"
     feature_detector(frame_path)
 
 
