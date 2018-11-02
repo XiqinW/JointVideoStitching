3e0f57d4b4c080064ffd1cf075b30ec04384f6a4
kiiyvan <plocircle@live.com>
2018-11-02 22:09:02 +0800

    skr

--- run.py
+++ run.py
@@ -3,11 +3,24 @@ import numpy as np
 
 FRAME_SIZE = (720, 1280, 3)
 GRID_ = (5, 5)
-FAST_FEATURE_THRESHOLD = [5 for i in range(GRID_[0] * GRID_[1])]
 MIN_FEATURE_NUM = 800
 
 
-def feature_detector(path):
+class FeatureDetector:
+
+    def __init__(self):
+        pass
+
+    def fast(self, threshold=5, on_=True):
+        return cv2.FastFeatureDetector_create(threshold, nonmaxSuppression=on_,
+                                              type=cv2.FAST_FEATURE_DETECTOR_TYPE_9_16)
+
+    def my_fast(self, threshold=5):
+        pass
+
+
+def detect_features(path):
+    feature_detector = FeatureDetector()
     original_frame = cv2.imread(path)
     frame = original_frame.copy()
 
@@ -23,9 +36,7 @@ def feature_detector(path):
     for i in range(GRID_[0]):
         for j in range(GRID_[1]):
             mask = np.zeros(frame.shape, np.uint8)
-            threshold = FAST_FEATURE_THRESHOLD[i * GRID_[1] + j]
-            fast = cv2.FastFeatureDetector_create(threshold, nonmaxSuppression=True,
-                                                  type=cv2.FAST_FEATURE_DETECTOR_TYPE_9_16)
+            fast = feature_detector.fast()
 
             mask[int(i * grid_size[0]):int((i + 1) * grid_size[0]),
             int(j * grid_size[1]):int((j + 1) * grid_size[1])] = 1
@@ -42,7 +53,7 @@ def feature_detector(path):
 
 def main():
     frame_path = "./0001.jpg"
-    feature_detector(frame_path)
+    detect_features(frame_path)
 
 
 if __name__ == "__main__":
